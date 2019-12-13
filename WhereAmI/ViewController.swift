79d430777404e1dfb334c0780f2760a1677afb4d
JWest2323 <jwest23@ufl.edu>
2019-12-13 14:23:08 -0600

    Initial Commit

---
+++ WhereAmI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereAmI
+//
+//  Created by Jamal West on 12/13/19.
+//  Copyright © 2019 Jamal West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
