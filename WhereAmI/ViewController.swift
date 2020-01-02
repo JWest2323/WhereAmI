a747ee52f7c8dc21d0d6033e8d1d01460b5441e4
JWest2323 <jwest23@ufl.edu>
2020-01-02 00:14:39 -0500

    stuck on globe view, will revisit later

--- WhereAmI/ViewController.swift
+++ WhereAmI/ViewController.swift
@@ -12,18 +12,22 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    @IBOutlet weak var mapKit: MKMapView!
-    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let locationManager = CLLocationManager()
         locationManager.requestAlwaysAuthorization()
         locationManager.requestWhenInUseAuthorization()
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
+            centerMapOnLocation(location: locationManager)
+            mapView.setRegion(coordinateRegion, animated: true)
         }
+        
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -31,6 +35,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
 }
+
+// code snippet to center map radius
+let regionRadius: CLLocationDistance = 1000
+func centerMapOnLocation(location: CLLocation) {
+    let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                              latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+  
+}
+
+// code snippet to show full globe in 3d view
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function to calculate the city and country
 //func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
